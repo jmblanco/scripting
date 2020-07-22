185f17f1109ec9c4db22d3509630021dc0c69b1f
Jose Miguel Blanco <jmblanco@universia.net>
2020-07-22 08:47:02 +0200

    FEAT: Update script

--- scripts/susetup.sh
+++ scripts/susetup.sh
@@ -3,17 +3,16 @@
 USAGE="$(basename "$0") [-h] [-u n] [-d]
 where:
     -h  show this help text
-    -s  user to config"
+    -s  user to config
+    -d  destiny"
 
 # Defaults
-USER=`whoami`
-
 while getopts 'hu:d:' OPTION; do
   case $OPTION in
    h) echo $USAGE
       exit ;;
    d) DRY_RUN=$OPTARG ;;
-   u) USER=$OPTARG ;;
+   u) CONFIG_USER=$OPTARG  ;;
    :) printf "missing argument for -%s\n" "$OPTARG" >&2
       echo "$USAGE" >&2
       exit 1 ;;
@@ -25,6 +24,11 @@ done
 
 [[ -z $DRY_RUN ]] && FILE="/etc/sudoers" || FILE=$DRY_RUN
 
-echo "Setup for user ${USER}"
-echo "${USER}  ALL=(ALL:ALL) ALL" >> ${FILE}
-echo "Setup complete!"
+if [ -z $CONFIG_USER ]
+then
+   echo "No user specified!"
+else
+   echo "Setup for user ${CONFIG_USER}"
+   echo "${CONFIG_USER}  ALL=(ALL:ALL) ALL" >> ${FILE}
+   echo "Setup complete!"
+fi
